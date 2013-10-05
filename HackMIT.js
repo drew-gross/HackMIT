22a86ff4e696bda640a112f6c602c17c79932666
Drew Gross <drew.a.gross@gmail.com>
2013-10-05 13:14:36 -0400

    pan the view

--- HackMIT.js
+++ HackMIT.js
@@ -7,9 +7,14 @@ if (Meteor.isClient) {
     'click input' : function () {
       // template data, if any, is available in 'this'
       google.load("earth", "1", {"other_params":"sensor=false", "callback":function(){
-        google.earth.createInstance("map3d", function (instance){
+        google.earth.createInstance("map3d", function (ge){
           //success
-          instance.getWindow().setVisibility(true);
+          ge.getWindow().setVisibility(true);
+          var lookAt = ge.getView().copyAsLookAt(ge.ALTITUDE_RELATIVE_TO_GROUND);
+          lookAt.setLatitude(36.584207);
+          lookAt.setLongitude(-121.754322);
+          lookAt.setRange(5000.0);
+          ge.getView().setAbstractView(lookAt);
         }, function(errorCode) {
           //failure
         });
