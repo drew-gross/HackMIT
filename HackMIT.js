bb511372cd79852fab14a352ca74e4017d30b9ae
Drew Gross <drew.a.gross@gmail.com>
2013-10-05 12:18:24 -0400

    added google earth view

--- HackMIT.js
+++ HackMIT.js
@@ -6,8 +6,13 @@ if (Meteor.isClient) {
   Template.hello.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+      google.load("earth", "1", {"other_params":"sensor=false", "callback":function(){
+        google.earth.createInstance("map3d", function (instance){
+          //success
+          instance.getWindow().setVisibility(true);
+        }, function(errorCode) {
+          //failure
+        });}});
     }
   });
 }
