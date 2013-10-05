00878d6f32d4637439e654327773b7e46c38eeae
Drew Gross <drew.a.gross@gmail.com>
2013-10-05 18:43:14 -0400

    structure for presenting

--- HackMIT.js
+++ HackMIT.js
@@ -24,13 +24,15 @@ if (Meteor.isClient) {
 
     $( "#getAlbum" ).submit(function( event ) {
     event.preventDefault();
-    console.log($('#albumSelect').val());
     var requestData = {
           access_token : user_access_token, 
           fields : "photos"
         }
     $.get('https://graph.facebook.com/' + $('#albumSelect').val(),requestData,function(data){
-          console.log(data);
+      var place = _.find(data.photos.data, function(photo) {
+        return _.has(photo, 'place');
+      }).place;
+      var SCSearchString = place.name + " " + place.location.city + " " + place.location.country + " traditional";
     })
     });
 
@@ -72,21 +74,33 @@ if (Meteor.isClient) {
   Template.hello.events({
     'click #make_map' : function () {
       // template data, if any, is available in 'this'
-      var lookAt = ge.getView().copyAsLookAt(ge.ALTITUDE_RELATIVE_TO_GROUND);
-      lookAt.setLatitude(36.584207);
-      lookAt.setLongitude(-121.754322);
-      lookAt.setRange(5000.0);
-      SC.get('/tracks', { q: 'Malaysia'}, function(tracks) {
-        SC.stream('/tracks/' + tracks[1].id, function(sound) {
-          SC.whenStreamingReady(function() {
-            ge.getView().setAbstractView(lookAt);
-            sound.play()
-          });
-        });
-      }); 
+      var mockList = 
+      [
+        {latitude: 37, longitude:-119, photos:[], SCSearchString: "Yosemite Valley"}, 
+        {latitude: 42, longitude: -71, photos:[], SCSearchString: "Boston"}
+      ];
+      present(mockList);
     }
   });
 
+}
+
+var present = function(list) {
+  if (list.length === 0) {
+    return;
+  };
+  var item = list.shift();
+  var lookAt = ge.getView().copyAsLookAt(ge.ALTITUDE_RELATIVE_TO_GROUND);
+  lookAt.setLatitude(item.latitude);
+  lookAt.setLongitude(item.longitude);
+  lookAt.setRange(5000.0);
+  SC.get('/tracks', {q: item.SCSearchString}, function(tracks) {
+    SC.stream('/tracks/' + tracks[0].id, function(sound) {
+      sound.play();
+      ge.getView().setAbstractView(lookAt);
+      present(list);
+    });
+  });
 }
 
 function loadAlbums() {
