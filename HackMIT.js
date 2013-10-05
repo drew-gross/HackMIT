2ea8df72110d22c0627331764e538bf0852baf3c
Jennifer Blight <jgblight@Jennifers-MacBook-Pro.local>
2013-10-05 16:38:46 -0400

    facebook stuff is working

--- HackMIT.js
+++ HackMIT.js
@@ -1,15 +1,86 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to HackMIT.";
-  };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+
+
+if (Meteor.isClient) {
+
+  var user_access_token = "none";
+
+  Meteor.startup(function() {
+
+    $( "#getAlbum" ).submit(function( event ) {
+    event.preventDefault();
+    console.log($('#albumSelect').val());
+    var requestData = {
+          access_token : user_access_token, 
+          fields : "photos"
+        }
+    $.get('https://graph.facebook.com/' + $('#albumSelect').val(),requestData,function(data){
+          console.log(data);
+    })
+    });
+
+
+  });
+
+
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '600241133350638', // App ID
+    channelUrl : '//hackmit.meteor.com/channel.html', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+
+  
+  FB.Event.subscribe('auth.authResponseChange', function(response) { 
+    if (response.status === 'connected') {
+      user_access_token = response.authResponse.accessToken;
+      loadAlbums();
+    } else if (response.status === 'not_authorized') {
+      FB.login(function(response){console.log(response);},{scope: 'user_photos'});
+    } else {
+      FB.login(function(response){console.log(response);},{scope: 'user_photos'});
     }
   });
+  };
+
+  // Load the SDK asynchronously
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+}
+
+function loadAlbums() {
+  FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    var requestData = {
+      access_token : response.authResponse.accessToken, 
+      fields : "name,albums"
+    }
+    $.get('https://graph.facebook.com/me',requestData,function(data){
+      console.log(data);
+      createAlbumForm(data.albums);
+    })
+  } else if (response.status === 'not_authorized') {
+    // the user is logged in to Facebook, 
+    // but has not authenticated your app
+  } else {
+    // the user isn't logged in to Facebook.
+  }
+ });
+}
+
+function createAlbumForm(albums) {
+  var albumSelect = $("#albumSelect");
+  for (var i = 0; i < albums.data.length; i++){
+    albumSelect.append('<option value=' + albums.data[i].id + '>' + albums.data[i].name + '</option>')
+  }
 }
 
 if (Meteor.isServer) {
