b9fc52a16e8b58b0313b91ab33873177bdaeb643
Drew Gross <drew.a.gross@gmail.com>
2013-10-05 15:48:33 -0400

    initialize in startup()

--- HackMIT.js
+++ HackMIT.js
@@ -1,3 +1,5 @@
+var ge;
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to HackMIT.";
@@ -6,33 +8,33 @@ if (Meteor.isClient) {
   Template.hello.events({
     'click #make_map' : function () {
       // template data, if any, is available in 'this'
-      google.load("earth", "1", {"other_params":"sensor=false", "callback":function(){
-        google.earth.createInstance("map3d", function (ge){
-          //success
-          ge.getWindow().setVisibility(true);
-          var lookAt = ge.getView().copyAsLookAt(ge.ALTITUDE_RELATIVE_TO_GROUND);
-          lookAt.setLatitude(36.584207);
-          lookAt.setLongitude(-121.754322);
-          lookAt.setRange(5000.0);
-          SC.initialize({
-            client_id:'d60a5d4319bb04cf49a854e98ec89c12'
-          });
-          SC.get('/tracks', { q: 'Malaysia'}, function(tracks) {
-            SC.stream('/tracks/' + tracks[1].id, function(sound) {
-              sound.play();
-              setTimeout(function() {
-                ge.getView().setAbstractView(lookAt);
-              }, 500);
-            });
-          }); 
-        }, function(errorCode) {
-          //failure
+      ge.getWindow().setVisibility(true);
+      var lookAt = ge.getView().copyAsLookAt(ge.ALTITUDE_RELATIVE_TO_GROUND);
+      lookAt.setLatitude(36.584207);
+      lookAt.setLongitude(-121.754322);
+      lookAt.setRange(5000.0);
+      SC.initialize({
+        client_id:'d60a5d4319bb04cf49a854e98ec89c12'
+      });
+      SC.get('/tracks', { q: 'Malaysia'}, function(tracks) {
+        SC.stream('/tracks/' + tracks[1].id, function(sound) {
+          sound.play();
+          setTimeout(function() {
+            ge.getView().setAbstractView(lookAt);
+          }, 500);
         });
-      }});
+      }); 
     }
   });
   Meteor.startup(function() {
     document.getElementsByTagName("body")[0].style.margin = "0";
+    google.load("earth", "1", {other_params: "sensor=false", callback: function() {
+      google.earth.createInstance("map3d", function(instance) {
+        ge = instance;
+      }, function(errorCode) {
+        //failure
+      });
+    }});
   });
 }
 
